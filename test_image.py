a8bb2d673d5fc5fde0f226a112fd8797faf58b80
aafan-kadri-123 <aafan.kadri123@gmail.com>
2020-09-19 20:51:45 +0530

    This project is created for facial recognition using haarcascade. 'faceRecognition' venv is used in local directory.

---
+++ test_image.py
@@ -0,0 +1,79 @@
+'''
+PyPower Projects
+Emotion Detection Using AI
+'''
+
+#USAGE : python test.py
+
+from keras.models import load_model
+from time import sleep
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
+import cv2
+import numpy as np
+
+face_classifier = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+classifier =load_model('./Emotion_Detection.h5')
+
+class_labels = ['Angry','Happy','Neutral','Sad','Surprise']
+
+# cap = cv2.VideoCapture('Zoom Video.mp4')
+img = cv2.imread('Happy_face_man.jpg')
+
+
+labels = []
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+faces = face_classifier.detectMultiScale(gray,1.3,5)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h,x:x+w]
+    roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+
+
+    if np.sum([roi_gray])!=0:
+        roi = roi_gray.astype('float')/255.0
+        roi = img_to_array(roi)
+        roi = np.expand_dims(roi,axis=0)
+
+    # make a prediction on the ROI, then lookup the class
+
+        preds = classifier.predict(roi)[0]
+        print("\nprediction = ",preds)
+        label=class_labels[preds.argmax()]
+        print("\nprediction max = ",preds.argmax())
+        print("\nlabel = ",label)
+        label_position = (x,y)
+        cv2.putText(img,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+        
+cv2.imwrite('outimg.jpg', img)
+cv2.imshow('Emotion Detector',img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
